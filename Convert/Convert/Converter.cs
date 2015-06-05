199f461c0dd1b08a298c84ab9002b503f399cf9a
mitsuhiro-nakamura <nakamura-m@sakailab.info>
2015-06-05 13:59:33 +0900

    2進数10進数変換プログラム(未完成)

---
+++ Convert/Convert/Converter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert
+{
+    class Converter
+    {
+        static void Main(string[] args)
+        {
+            string s;
+
+            // 10進数→2進数へと変換する
+            s = Convert.ToString(128, 2);
+            Console.WriteLine(s);
+        }
+    }
+}
